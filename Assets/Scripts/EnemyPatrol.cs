d38fdf79e4d3170d8fc31b512cbb818c14431ed1
Mawathilde <mawathilde@gmail.com>
2021-01-17 15:12:20 +0100

    Add player damage

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -6,6 +6,8 @@ public class EnemyPatrol : MonoBehaviour
     public float speed;
     public Transform[] waypoints;
 
+    public int damageOnCollision;
+
     public SpriteRenderer graphics;
     private Transform target;
     private int destPoint;
@@ -14,7 +16,6 @@ public class EnemyPatrol : MonoBehaviour
     {
         target = waypoints[0];
     }
-
 
     void Update()
     {
@@ -25,6 +26,15 @@ public class EnemyPatrol : MonoBehaviour
             destPoint = (destPoint + 1) % waypoints.Length;
             target = waypoints[destPoint];
             graphics.flipX = !graphics.flipX;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = collision.transform.GetComponent<PlayerHealth>();
+            playerHealth.TakeDamage(damageOnCollision);
         }
     }
 
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -6,6 +7,11 @@ public class PlayerHealth : MonoBehaviour
     public int maxHealth = 100;
     public int currentHealth;
 
+    public float invisiblityFlashDelay = 0.2f;
+    public float invisiblityDuration = 2.5f;
+    public bool isInvincible = false;
+
+    public SpriteRenderer graphics;
     public HealthBar healthBar;
 
     void Start()
@@ -16,16 +22,39 @@ public class PlayerHealth : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.H))
+        if (Input.GetKeyDown(KeyCode.H))
         {
             TakeDamage(20);
         }
     }
 
-    void TakeDamage(int damage)
+    public void TakeDamage(int damage)
     {
-        currentHealth -= damage;
-        healthBar.SetHealth(currentHealth);
+        if (!isInvincible)
+        {
+            currentHealth -= damage;
+            healthBar.SetHealth(currentHealth);
+            isInvincible = true;
+            StartCoroutine(InvisibilityFlash());
+            StartCoroutine(HandleInvisibilityDelay());
+        }
+    }
+
+    public IEnumerator InvisibilityFlash()
+    {
+        while (isInvincible)
+        {
+            graphics.color = new Color(1f, 1f, 1f, 0f);
+            yield return new WaitForSeconds(invisiblityFlashDelay);
+            graphics.color = new Color(1f, 1f, 1f, 1f);
+            yield return new WaitForSeconds(invisiblityFlashDelay);
+        }
+    }
+
+    public IEnumerator HandleInvisibilityDelay()
+    {
+        yield return new WaitForSeconds(invisiblityDuration);
+        isInvincible = false;
     }
 
 }
