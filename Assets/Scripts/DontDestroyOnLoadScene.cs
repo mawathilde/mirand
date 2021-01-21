4ce3040556f96e201b9989f109f163db49ab6d98
Mawathilde <mawathilde@gmail.com>
2021-01-21 22:04:08 +0100

    Add multiple scenes system

---
+++ Assets/Scripts/DontDestroyOnLoadScene.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DontDestroyOnLoadScene : MonoBehaviour
+{
+
+    public GameObject[] objects;
+
+    private void Awake()
+    {
+        foreach (var element in objects)
+        {
+            DontDestroyOnLoad(element);
+        }
+    }
+
+}
---
+++ Assets/Scripts/LoadSpecificScene.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSpecificScene : MonoBehaviour
+{
+
+    public string targetScene;
+    private Animator fadeSystem;
+
+    private void Awake()
+    {
+        fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            StartCoroutine(loadNextScene());
+        }
+    }
+
+    public IEnumerator loadNextScene()
+    {
+        fadeSystem.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(targetScene);
+    }
+
+}
